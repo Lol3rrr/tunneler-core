5a5bda8d72111507965271fed086132ba10abe4d
lol3rrr <s.loler03@gmail.com>
2021-02-11 08:59:54 +0100

    Moved some stuff in the client-manager around

--- src/server/client/client_manager.rs
+++ src/server/client/client_manager.rs
@@ -2,6 +2,7 @@ use crate::server::client::Client;
 
 pub struct ClientManager {
     index: std::sync::atomic::AtomicU64,
+    client_count: std::sync::atomic::AtomicU64,
     clients: std::sync::Mutex<Vec<Client>>,
 }
 
@@ -16,59 +17,55 @@ impl ClientManager {
     pub fn new() -> ClientManager {
         ClientManager {
             index: std::sync::atomic::AtomicU64::new(0),
+            client_count: std::sync::atomic::AtomicU64::new(0),
             clients: std::sync::Mutex::new(Vec::new()),
         }
     }
 
-    // TODO implement a proper client selection instead of just returning
-    // the first one
     pub fn get(&self) -> Option<Client> {
-        let clients_data = self.clients.lock().unwrap();
-        if clients_data.len() == 0 {
+        if self.client_count() == 0 {
             return None;
         }
 
-        let index =
-            self.index.load(std::sync::atomic::Ordering::SeqCst) as usize % clients_data.len();
-        let client = match clients_data.get(index) {
-            Some(c) => c.clone(),
-            None => {
-                return None;
+        let clients_data = self.clients.lock().unwrap();
+        let raw_index = self.index.load(std::sync::atomic::Ordering::SeqCst);
+        let client_count = self.client_count.load(std::sync::atomic::Ordering::SeqCst);
+
+        let index = raw_index % client_count;
+        match clients_data.get(index as usize) {
+            Some(c) => {
+                self.index.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                Some(c.clone())
             }
-        };
-        drop(clients_data);
-
-        self.index.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-
-        Some(client)
+            None => None,
+        }
     }
 
     /// Adds a new client connection to the List of connections
     ///
     /// Params:
-    /// * client: The client to add
+    /// * client: The Client to add
     ///
     /// Returns:
     /// This function returns the new number of clients managed
     /// by this
-    pub fn add(&self, client: Client) -> usize {
+    pub fn add(&self, client: Client) {
         let mut clients_data = self.clients.lock().unwrap();
         clients_data.push(client);
-        let count = clients_data.len();
         drop(clients_data);
-
-        count
+        self.client_count
+            .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
     }
 
     /// This is used to remove a client connection again
     ///
     /// Params:
-    /// * id: The ID of the connection to remove
+    /// * id: The ID of the Client-Connection to remove
     ///
     /// Returns:
     /// This function returns the new number of clients managed
     /// by this
-    pub fn remove_con(&self, id: u32) -> usize {
+    pub fn remove(&self, id: u32) {
         let mut client_data = self.clients.lock().unwrap();
         let mut remove_index: Option<usize> = None;
         for (index, client) in client_data.iter().enumerate() {
@@ -82,13 +79,16 @@ impl ClientManager {
             None => {}
             Some(i) => {
                 client_data.remove(i);
+                self.client_count
+                    .fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
             }
         };
 
-        let count = client_data.len();
-
         drop(client_data);
+    }
 
-        count
+    /// Returns the number of currently connected clients
+    pub fn client_count(&self) -> u64 {
+        self.client_count.load(std::sync::atomic::Ordering::SeqCst);
     }
 }
